402005b988ce6674573774f4763a484e453fd80f
Heiner Pöpping <heiner.poepping@t-online.de>
2018-08-30 12:44:08 +0200

    Improved Reducer and cleaned up code

--- src/components/result/ResultList.jsx
+++ src/components/result/ResultList.jsx
@@ -1,55 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fromJS } from 'immutable';
 import ResultListItem from './ResultListItem';
 import './ResultList.scss';
 import { SHOW_MORE, NOTHING_FOUND } from '../../constants/text';
-import { loadSites as loadSitesAction } from '../../actions/fetchSites';
 
-class ResultList extends React.Component {
-    static propTypes = {
-        sites: PropTypes.instanceOf(Array).isRequired,
-        reachedEnd: PropTypes.bool,
-        callback: PropTypes.func.isRequired
-    }
-
-    static defaultProps = {
-        reachedEnd: true
-    }
-
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    render() {
-        const listItems = this.props.sites.map(t => <ResultListItem key={t.siteId} {...t} />);
-        return (
-            <div className={`accordion__body color--${chayns.env.parameters.colormode}`} >
-                {listItems}
-                {!this.props.reachedEnd ?
-                    <div className="accordion__content">
-                        <div style={{ textAlign: 'right' }} >
-                            <a href="#" onClick={this.props.callback} onKeyPress={() => undefined} >{ SHOW_MORE }</a>
-                        </div>
-                    </div>
-                    : undefined}
-                {this.props.sites.length === 0 ?
-                    <div className="accordion__content">{NOTHING_FOUND}</div>
-                    : undefined}
+const ResultList = ({ sites, reachedEnd, callback }) => (
+    <div className={`accordion__body color--${chayns.env.parameters.colormode}`} >
+        {sites.map(t => <ResultListItem key={t.siteId} {...t} />)}
+        {!reachedEnd ?
+            <div className="accordion__content">
+                <div style={{ textAlign: 'right' }} >
+                    <a href="#" onClick={callback} onKeyPress={() => undefined} >{ SHOW_MORE }</a>
+                </div>
             </div>
-        );
-    }
-}
+            : undefined}
+        {sites.length === 0 ?
+            <div className="accordion__content">{NOTHING_FOUND}</div>
+            : undefined}
+    </div>
+);
 
-const mapDispatchToProps = dispatch => ({
-    addUser: (searchString, start, take) => dispatch(loadSitesAction(fromJS(searchString, start, take))),
-});
-
-const mapStateToProps = (state, ownProps) => {
-    console.log(state);
-    return Object.assign({}, ownProps, { sites: state.fetchSites.size > 0 ? state.fetchSites.get('sites') : [] });
+ResultList.propTypes = {
+    sites: PropTypes.instanceOf(Array).isRequired,
+    reachedEnd: PropTypes.bool,
+    callback: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ResultList);
+ResultList.defaultProps = {
+    reachedEnd: true
+};
+
+export default ResultList;
--- src/reducers/fetchSites.js
+++ src/reducers/fetchSites.js
@@ -1,13 +1,22 @@
-import { Map } from 'immutable';
-import { SAVE_SERVER_SITES } from '../actions/fetchSites';
+import { LOAD_SITES_NEW, LOAD_SITES_APPEND } from '../actions/fetchSites';
 
-export const initialState = Map();
+export const initialState = { searchString: 'chayns', start: 0, sites: [] };
 
 const fetchSites = (state = initialState, action) => {
     switch (action.type) {
-        case SAVE_SERVER_SITES:
-            return state.set('sites', action.data.sites)
-            .set('reachedEnd', action.data.reachedEnd);
+        case LOAD_SITES_NEW:
+            return Object.assign({}, state, {
+                sites: action.data.sites,
+                searchString: action.data.searchString,
+                start: action.data.sites.length,
+                reachedEnd: action.data.reachedEnd
+            });
+        case LOAD_SITES_APPEND:
+            return Object.assign({}, state, {
+                sites: state.sites.concat(action.data.sites),
+                start: state.start + action.data.sites.length,
+                reachedEnd: action.data.reachedEnd
+            });
         default:
             return state;
     }
